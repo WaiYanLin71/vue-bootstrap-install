68fedbf178f44a27aee7dbc9815c448b1be8662b
WaiYanLin71 <scm.waiyanlinn@gmail.com>
2022-12-16 14:48:11 +0630

    initial

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+import './scss/app.scss'
+import 'bootstrap'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
